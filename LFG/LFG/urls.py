24f0870b5c1f1ba00b60f2d72dda2e56736c3367
Luis Isarría <luisisarria@MacBook-Pro-de-Luis.local>
2022-09-01 18:08:42 +0200

    urls added

--- LFG/LFG/urls.py
+++ LFG/LFG/urls.py
@@ -1,7 +1,6 @@
-"""LFG URL Configuration
-
+"""lfg URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +13,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin/', include('api.urls')),
+    path('', include('rest_auth.urls')),
+    path('registration/', include('rest_auth.registration.urls')),
 ]
